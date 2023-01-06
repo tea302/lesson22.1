e3d25d407afab3cb928de619a81d7edc1300f77f
evgeniatimoshkina <tea302@yandex.ru>
2023-01-06 14:30:06 +0400

    r

---
+++ part1/practice/bad_smell_doubles/main_new.py
@@ -0,0 +1,11 @@
+class SomeClass:
+    def __init__(self):
+        self.lst = [3, 2, 1, 4, 2, 1]
+
+    def sorted_func(self, reverse: bool = False):
+        return sorted(self.lst, reverse=reverse)
+
+
+if __name__ == "__main__":
+    some_inst = SomeClass()
+    print(some_inst.sorted_func())
